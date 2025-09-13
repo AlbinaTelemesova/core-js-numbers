c8c9d25f15aa7887f7e5a351a6cbe9ce89db8f9a
AlbinaT <telemesova96@gmail.com>
2025-09-14 00:46:22 +0500

    feat: add solution of 11 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -198,8 +198,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const divisor = [];
+  let i = 1;
+  while (i <= n) {
+    if (n % i === 0) {
+      divisor.push(i);
+    }
+    i += 1;
+  }
+  return divisor.length <= 2;
 }
 
 /**
